fa8e209c8bd8a95727721a8477c0ce90efb7f547
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-02-22 13:45:46 -0500

    documentation

---
+++ Tipton_processing.py
@@ -0,0 +1,13 @@
+# Written by: Natalie Tipton
+# Advisor: Dr. Samhita Rhodes
+#
+# Created on: February 22, 2020
+#
+# Description: Signal Processing of COM and COP
+#   during stability testing in neuronormative
+#   subjects.
+#
+# Last updated: February 22, 2020
+
+import matplotlib.pyplot as plt
+import numpy as np
