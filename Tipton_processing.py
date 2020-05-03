e87863dd56495dbf740ac5bd0528e81fa5e3bd70
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-05-03 16:49:01 -0600

    cross corr

--- Tipton_processing.py
+++ Tipton_processing.py
@@ -25,7 +25,7 @@ fs_cop = 1200
 t_cop = np.arange(0, 30, 1 / fs_cop)
 fs_com = 120
 t_com = np.arange(0, 30, 1 / fs_com)
-filepath = "/Users/natalietipton/Code/Data/SB01/SB01Trial_19.csv"
+filepath = "/Users/natalietipton/Code/Data/SB01/SB01Trial_02.csv"
 
 # reads data in from .csv files for data with feet together, one force plate
 # returns: cx = CoP x position (AP), cy = CoP y position (ML)
@@ -48,7 +48,7 @@ def read_data_onefp(filepath, row_start, cols, num_data, rows_skip):
     cx = data["Cx"].values.tolist()
     cy = data["Cy"].values.tolist()
 
-    return cx, cy
+    return cx, cy, data["Cx"]
 
 
 def read_data_com(filepath, row_start, cols, num_data, rows_skip):
@@ -178,11 +178,11 @@ def collect_approx_entropy(x):
 
 if __name__ == "__main__":
 
-    # x_cop, y_cop = read_data_onefp(filepath, 3, ["Cx", "Cy"], 36000, [4])
+    x_cop, y_cop, x_cop_df = read_data_onefp(filepath, 3, ["Cx", "Cy"], 36000, [4])
     x_com, y_com, z_com = read_data_com(filepath, 36009, ["X", "Y", "Z"], 3600, [36010])
-    x_cop, y_cop = read_data_twofp(
-        filepath, 3, ["Fz", "Cx", "Cy", "Fz.1", "Cx.1", "Cy.1"], 36000, [4]
-    )
+    # x_cop, y_cop = read_data_twofp(
+    #     filepath, 3, ["Fz", "Cx", "Cy", "Fz.1", "Cx.1", "Cy.1"], 36000, [4]
+    # )
 
     n_cop = len(x_cop)
     n_com = len(x_com)
@@ -519,3 +519,19 @@ if __name__ == "__main__":
     #     [0, 30],
     #     None,
     # )
+
+    x_cop_df_resample = []
+    for i in range(0, n_com):
+        x_cop_df_resample.append(x_cop_standard[i * 10])
+
+    cross_x_cop_com = np.correlate(x_cop_df_resample, x_com_standard, mode="full")
+
+    plot(
+        t_corr_com,
+        cross_x_cop_com,
+        "time (s)",
+        "cross correlation",
+        "X CoM and X CoP cross correlation",
+        [0, 30],
+        None,
+    )
