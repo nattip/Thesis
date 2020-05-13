52699c086c07fc9eac6a130db81d0e40b64d163b
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-05-13 09:44:11 -0600

    linreg

--- regr.py
+++ regr.py
@@ -19,6 +19,7 @@ import analysis_lib as an
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import PolynomialFeatures
+from scipy.stats import norm
 
 
 ROOT = "/Users/natalietipton/Code/center_of_pressure/data"
@@ -30,6 +31,32 @@ regression_y_together = pd.DataFrame()
 
 all_x_data = []
 all_y_data = []
+
+# https://github.com/NathanMaton/prediction_intervals/blob/master/sklearn_prediction_interval_extension.ipynb
+# returns a prediction interval for a linear regression prediction
+# Inputs:
+#   prediction = single prediction
+#   y_test = test data that is being used for the regression
+#   test_predictions = entire list of regression predictions
+#   pi = confidence intervale
+# Outputs:
+#   lower = a single value of the lower bound
+#   upper = a single value of the upper bound
+def get_prediction_interval(prediction, y_test, test_predictions, pi=0.95):
+    # get standard deviation of y_test
+    sum_errs = np.sum((y_test - test_predictions) ** 2)
+    stdev = np.sqrt(1 / (len(y_test) - 2) * sum_errs)
+
+    # get interval from standard deviation
+    one_minus_pi = 1 - pi
+    ppf_lookup = 1 - (one_minus_pi / 2)
+    z_score = norm.ppf(ppf_lookup)
+    interval = z_score * stdev
+
+    # generate prediction interval lower and upper bound
+    lower, upper = prediction - interval, prediction + interval
+
+    return lower, upper
 
 
 def poly(X, Y):
@@ -47,23 +74,21 @@ def poly(X, Y):
     print(f"RMSE: {rmse}")
     print(f"R2: {r2}")
 
-    # bound = 1 - r2 / 2
-    # X_upper = []
-    # X_lower = []
-    # for yy in y_polynomial_predictions:
-    #     for x in yy:
-    #         X_upper.append(x + x * bound)
-    #         X_lower.append(x - x * bound)
-    # X_upper = np.array(X_upper)
-    # X_lower = np.array(X_lower)
+    X_upper = []
+    X_lower = []
+
+    for prediction in y_polynomial_predictions:
+        lower, upper = get_prediction_interval(prediction, Y, y_polynomial_predictions)
+        X_upper.append(upper)
+        X_lower.append(lower)
 
     plt.plot(X, Y)
     sort_axis = operator.itemgetter(0)
     sorted_zip = sorted(zip(X, y_polynomial_predictions), key=sort_axis)
     x, y_polynomial_predictions = zip(*sorted_zip)
     plt.plot(x, y_polynomial_predictions, color="m")
-    # plt.plot(x, X_upper, color="r")
-    # plt.plot(x, X_lower, color="g")
+    plt.plot(x, X_upper, color="r")
+    plt.plot(x, X_lower, color="g")
     plt.show()
 
     # return y_polynomial_predictions
@@ -94,20 +119,18 @@ if __name__ == "__main__":
         for file in dirs[directory]:
             number = int(file[10:12])
 
-            if number > 26:
-                df = pd.read_csv(os.path.join(directory, file), index_col=False)
+            # if number > 11:
+            df = pd.read_csv(os.path.join(directory, file), index_col=False)
 
-                df = df.to_numpy()
-                values = np.delete(df, 0, 1)
-                Xaxis, Yaxis = values.T
+            df = df.to_numpy()
+            values = np.delete(df, 0, 1)
+            Xaxis, Yaxis = values.T
 
-                Xaxis = an.standardize(Xaxis)
-                Yaxis = an.standardize(Yaxis)
-                # Xaxis = Xaxis[:, np.newaxis]
-                # Yaxis = Yaxis[:, np.newaxis]
+            # Xaxis = an.standardize(Xaxis)
+            # Yaxis = an.standardize(Yaxis)
 
-                all_x_data.append(Xaxis)
-                all_y_data.append(Yaxis)
+            all_x_data.append(Xaxis)
+            all_y_data.append(Yaxis)
 
     all_x_data = np.array(all_x_data)
     all_y_data = np.array(all_y_data)
@@ -116,25 +139,11 @@ if __name__ == "__main__":
     avg_y_data = np.mean(all_y_data, axis=0)
     # print(avg_x_data)
     an.plot(
-        t_cop,
-        avg_x_data,
-        "time",
-        "signal",
-        "Average of all Feet Tandem, EC, DB, X Axis",
-        None,
-        None,
+        t_cop, avg_x_data, "time", "signal", "Average of all X Axis", None, None,
     )
     an.plot(
-        t_cop,
-        avg_y_data,
-        "time",
-        "signal",
-        "Average of all Feet Tandem, EC, DB, Y Axis",
-        None,
-        None,
+        t_cop, avg_y_data, "time", "signal", "Average of all Y Axis", None, None,
     )
-    #         regression_x = poly(t_cop, Xaxis)
-    #         regression_y = poly(t_cop, Yaxis)
 
     avg_x_data = avg_x_data[:, np.newaxis]
     avg_y_data = avg_y_data[:, np.newaxis]
@@ -155,40 +164,39 @@ if __name__ == "__main__":
     #         )
 
     # print(regression_x_together)
-################################
-# def poly(X, Y):
-#     polynomial_features = PolynomialFeatures(degree=9)
-#     X_polynomial = polynomial_features.fit_transform(X)
+    ################################
+    # def poly(X, Y):
+    #     polynomial_features = PolynomialFeatures(degree=9)
+    #     X_polynomial = polynomial_features.fit_transform(X)
 
-#     model = LinearRegression()
-#     model.fit(X_polynomial, Y)
+    #     model = LinearRegression()
+    #     model.fit(X_polynomial, Y)
 
-#     y_polynomial_predictions = model.predict(X_polynomial)
+    #     y_polynomial_predictions = model.predict(X_polynomial)
 
-#     rmse = np.sqrt(mean_squared_error(Y, y_polynomial_predictions))
-#     r2 = r2_score(Y, y_polynomial_predictions)
-#     print(f"RMSE: {rmse}")
-#     print(f"R2: {r2}")
+    #     rmse = np.sqrt(mean_squared_error(Y, y_polynomial_predictions))
+    #     r2 = r2_score(Y, y_polynomial_predictions)
+    #     print(f"RMSE: {rmse}")
+    #     print(f"R2: {r2}")
 
-#     plt.plot(X, Y)
-#     sort_axis = operator.itemgetter(0)
-#     sorted_zip = sorted(zip(X, y_polynomial_predictions), key=sort_axis)
-#     x, y_polynomial_predictions = zip(*sorted_zip)
-#     plt.plot(x, y_polynomial_predictions, color="m")
-#     # plt.plot(x, X_upper, color="r")
-#     # plt.plot(x, X_lower, color="g")
-#     plt.show()
+    #     plt.plot(X, Y)
+    #     sort_axis = operator.itemgetter(0)
+    #     sorted_zip = sorted(zip(X, y_polynomial_predictions), key=sort_axis)
+    #     x, y_polynomial_predictions = zip(*sorted_zip)
+    #     plt.plot(x, y_polynomial_predictions, color="m")
+    #     # plt.plot(x, X_upper, color="r")
+    #     # plt.plot(x, X_lower, color="g")
+    #     plt.show()
+    # if __name__ == "__main__":
 
+    # df = pd.read_csv(f"{ROOT}/SB02/SB02_Trial02_norm_spliced.csv", index_col=False)
 
-# if __name__ == "__main__":
+    # df = df.to_numpy()
+    # values = np.delete(df, 0, 1)
+    # Xaxis, Yaxis = values.T
+    # Xaxis = Xaxis[:, np.newaxis]
+    # Yaxis = Yaxis[:, np.newaxis]
+    # t_cop = t_cop[:, np.newaxis]
 
-#     df = pd.read_csv(f"{ROOT}/SB02/SB02_Trial02_norm_spliced.csv", index_col=False)
-
-#     df = df.to_numpy()
-#     values = np.delete(df, 0, 1)
-#     Xaxis, Yaxis = values.T
-#     Xaxis = Xaxis[:, np.newaxis]
-#     Yaxis = Yaxis[:, np.newaxis]
-
-#     poly(t_cop, Xaxis)
-#     poly(t_cop, Yaxis)
+    # poly(t_cop, Xaxis)
+    # poly(t_cop, Yaxis)
